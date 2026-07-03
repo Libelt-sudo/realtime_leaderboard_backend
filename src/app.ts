e2ca30986af74aff35bb3cad3cf9909b0f8e3ffc
Libelt-sudo <seibet.07@gmail.com>
2026-07-03 19:55:01 +0100

    Changed ports for DB and changed docker compose file to only launch the app, instead of postgres and redis-server because I will be running them locally on the vps.

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ const PORT = 3000;
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5174"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
